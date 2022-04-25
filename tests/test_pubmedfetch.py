1f436aabf8a83403d493190a1439e575d2c2725d
Jeffrey Wong <jvwong@gmail.com>
2022-04-25 16:02:36 -0400

    Sample tests

--- tests/test_pubmedfetch.py
+++ tests/test_pubmedfetch.py
@@ -1,5 +1,22 @@
 from pubmedfetch import __version__
+from pubmedfetch.pubmedfetch import add
+import os
+
+
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "data",
+)
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_fixture(shared_datadir):
+    contents = (shared_datadir / "sample_data.txt").read_text()
+    assert contents == "Hello World!\n"
+
+
+def test_add_one():
+    assert add(1, 2) == 3
