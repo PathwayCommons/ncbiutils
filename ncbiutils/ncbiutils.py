b1032569bb6fb6a1d96b28d60594bb1c70c6e4b4
Jeffrey Wong <jvwong@gmail.com>
2022-04-27 17:00:21 -0400

    First instance of Base Eutil class

--- ncbiutils/ncbiutils.py
+++ ncbiutils/ncbiutils.py
@@ -1,120 +1,55 @@
-# from enum import Enum
+from pydantic import BaseModel, HttpUrl, validator
+from typing import ClassVar
+from ncbiutils.types import HttpMethodEnum
+from ncbiutils.http import safe_requests
 
-# from typing import ClassVar, List
-
-# from pydantic import BaseModel, HttpUrl, EmailStr, validator
-
-# import requests
 # from loguru import logger
 
 
-# class HttpMethodEnum(str, Enum):
-#     get = 'GET'
-#     post = 'POST'
+class Eutil(BaseModel):
+    """
+    A base class for other NCBI E-Utilities
+
+    Class attributes
+    ----------
+    base_url : HttpUrl
+        Base URL for the various NCBI E-Utilities
+    retmax_limit : int
+        Maximum number of records that can be returned
+
+    Attributes
+    ----------
+    retstart : int
+        Index before the first record to return (default 0)
+    retmax : int
+        Maximum number of records to return (default 10000)
+    api_key : str
+        Key for NCBI E-Utilities
+
+    """
+
+    base_url: ClassVar[HttpUrl] = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+    retmax_limit: ClassVar[int] = 10000
+
+    retstart: int = 0
+    retmax: int = retmax_limit
+    api_key: str = None
+
+    @validator('retmax')
+    def retmax_is_nonneg_lt_limit(cls, v):
+        if v < 0 or v > cls.retmax_limit:
+            raise ValueError(f'Must be positive number less than {cls.retmax_limit}')
+        return v
 
 
-# class EutilEnum(str, Enum):
-#     efetch = 'efetch'
-
-
-# class DbEnum(str, Enum):
-#     pubmed = 'pubmed'
-
-
-# class RetModeEnum(str, Enum):
-#     text = 'text'
-#     xml = 'xml'
-#     asn = 'asn.1'
-
-
-# class RetTypeEnum(str, Enum):
-#     uilist = 'uilist'
-#     full = 'full'
-#     abstract = 'abstract'
-#     docsum = 'docsum'
-#     medline = 'medline'
-
-
-# class Eutil(BaseModel):
-#     """
-#     A class that holds values related to various NCBI E-Utilities.
-
-#     Class attributes
-#     ----------
-#     name : str
-#         Package name
-#     version : str
-#         Package version
-#     package_url : HttpUrl
-#         URL for package repsitory
-#     admin_email : EmailStr
-#         Email for package maintainer
-#     base_url : HttpUrl
-#         Base URL for the various NCBI E-Utilities
-#     retmax_limit : int
-#         Maximum number of records that can be returned
-
-#     Attributes
-#     ----------
-#     http_request_timeout_seconds : int
-#         Timeout in seconds for server response (default 5)
-#     retstart : int
-#         Index before the first record to return (default 0)
-#     retmax : int
-#         Maximum number of records to return (default 10000)
-#     api_key : str
-#         API key for requests up to 10/sec
-
-#     """
-
-#     name: ClassVar[str] = 'ncbiutils'
-#     version: ClassVar[str] = '0.1.0'
-#     package_url: ClassVar[HttpUrl] = 'https://github.com/jvwong/ncbiutils'
-#     admin_email: ClassVar[EmailStr] = 'info@biofactoid.org'
-#     base_url: ClassVar[HttpUrl] = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
-#     retmax_limit: ClassVar[int] = 10000
-
-#     http_request_timeout_seconds: int = 5
-#     retstart: int = 0
-#     retmax: int = retmax_limit
-#     api_key: str = None
-
-#     @validator('retmax')
-#     def retmax_is_nonneg_lt_limit(cls, v):
-#         if v < 0 or v > cls.retmax_limit:
-#             raise ValueError(f'Must be positive number less than {cls.retmax_limit}')
-#         return v
-
-#     def _safe_request(self, url: HttpUrl, method: HttpMethodEnum = HttpMethodEnum.get, headers={}, **opts):
-#         user_agent = f'{self.name}/{self.version} ({self.package_url};mailto:{self.admin_email})'
-#         request_headers = {'user-agent': user_agent}
-#         request_headers.update(headers)
-#         try:
-#             r = requests.request(
-#                 method, url, headers=request_headers, timeout=self.http_request_timeout_seconds, **opts
-#             )
-#             r.raise_for_status()
-#         except requests.exceptions.Timeout as e:
-#             logger.error(f'Timeout error {e}')
-#             raise
-#         except requests.exceptions.HTTPError as e:
-#             logger.error(f'HTTP error {e}; status code: {r.status_code}')
-#             raise
-#         except requests.exceptions.RequestException as e:
-#             logger.error(f'Error in request {e}')
-#             raise
-#         else:
-#             return r
-
-#     def _get_eutil_response(self, url: HttpUrl, **opts):
-#         """Call one of the NCBI EUTILITIES and returns data as Python objects."""
-
-#         eutils_params = {'retstart': self.retstart, 'retmax': self.retmax}
-#         eutils_params.update(opts)
-#         if self.api_key is not None:
-#             eutils_params.update({'api_key': self.api_key})
-#         eutilResponse = self._safe_request(url, HttpMethodEnum.post, files=eutils_params)
-#         return eutilResponse
+    def _get_eutil_response(self, url: HttpUrl, **opts):
+        """Call one of the NCBI E-Utilities and return a requests.Reponse"""
+        params = {'retstart': self.retstart, 'retmax': self.retmax}
+        params.update(opts)
+        if self.api_key:
+            params.update({'api_key': self.api_key})
+        err, response = safe_requests(url, method = HttpMethodEnum.POST, files = params)
+        return err, response
 
 
 # class Fetcher(Eutil):
@@ -162,9 +97,9 @@
 #         return self._fetch(db=self.db, id=id, **pubmed_opts)
 
 
-class HelloWorld:
-    text = 'Hello World!'
+# class HelloWorld:
+#     text = 'Hello World!'
 
-    def load_data(self):
-        data = 2
-        return data
+#     def load_data(self):
+#         data = 2
+#         return data
--- tests/test_ncbiutils.py
+++ tests/test_ncbiutils.py
@@ -1,45 +1,51 @@
-from ncbiutils.ncbiutils import HelloWorld
+import pytest
+from ncbiutils.ncbiutils import Eutil
 
-# NCBI_EUTILS_BASE_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
-
-
-# class TestMedlineAttributes:
-#     key = 'somekey'
-#     medline = Medline(api_key=key)
-
-#     def test_inherits_class_base_url(self):
-#         assert Medline.base_url == f'{NCBI_EUTILS_BASE_URL}'
-
-#     def test_class_efetch_url(self):
-#         assert Medline.efetch_url == f'{NCBI_EUTILS_BASE_URL}efetch.fcgi'
-
-#     def test_set_api_key(self):
-#         assert self.medline.api_key == self.key
+NCBI_EUTILS_BASE_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
 
 
-# class TestMedlineFetchIntegration:
-#     medline = Medline()
-
-#     def test_fetch_valid_uids(self):
-#         uids = ['31827641', '31772623', '31766097']
-#         response = self.medline.get(uids)
-#         assert response.status_code == 200
-
-#     def test_fetch_invalid_uids(self):
-#         uids = ['00000000']
-#         with pytest.raises(Exception):
-#             self.medline.get(uids)
+#############################
+#   Unit tests
+#############################
 
 
-class TestHelloWorld:
-    text = HelloWorld.text
+class TestEutilClass:
+    def test_class_attributes(self):
+        assert Eutil.base_url == NCBI_EUTILS_BASE_URL
 
-    def test_text(self):
-        assert self.text == 'Hello World!'
+    def test_set_api_key(self):
+        key = 'somekey'
+        eutil = Eutil(api_key=key)
+        assert eutil.api_key == key
 
-    def test_data(self, mocker):
-        mockData = 3
-        mocker.patch('tests.test_ncbiutils.HelloWorld.load_data', return_value = mockData)
-        h = HelloWorld()
-        data = h.load_data()
-        assert data == mockData
+    def test_set_invalid_retmax(self):
+        bigger_than_retmax_limit = Eutil.retmax_limit + 1
+        with pytest.raises(Exception):
+            Eutil(retmax=bigger_than_retmax_limit)
+
+
+#############################
+#   Integration tests
+#############################
+
+
+@pytest.mark.parametrize(
+    'id, db, expected', [('31827641,31772623,31766097', 'pubmed', 200), ('00000000', 'pubmed', 400)]
+)
+class TestEutilFetchIntegration:
+    eutil = Eutil()
+
+    def test_efetch_valid_uids(self, id, db, expected):
+        url = f'{Eutil.base_url}efetch.fcgi'
+        opts = {'id': id, 'db': db}
+        _, response = self.eutil._get_eutil_response(url, **opts)
+        assert response.status_code == expected
+
+
+# class TestHelloWorld:
+#     def test_data(self, mocker):
+#         mockData = 3
+#         mocker.patch('tests.test_ncbiutils.HelloWorld.load_data', return_value = mockData)
+#         h = HelloWorld()
+#         data = h.load_data()
+#         assert data == mockData
